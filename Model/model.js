abc7cc14eef2dda0e9852654a29dfb5317bca5d1
it22307330 <it22307330@my.sliit.lk>
2023-03-15 23:15:03 +0530

    modify user

---
+++ Model/model.js
@@ -0,0 +1,25 @@
+<div class="container">
+
+            <form action="" class="form">
+
+            <div class="heading">
+                <h1>Log In</h1>
+                <p>Welcome to your finacial smart decision making</p>
+            </div>
+        
+            <div class="row mb-3 align-item-center">
+                <label for="formGroupEampleInput" class="form-label" id="username">Username</label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="User Name" required>
+                <div class="username-error"></div>
+            </div>
+            <div class="row mb-3 align-item-center">
+                <label for="formGroupEampleInput" class="form-label" id="password">Password</label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Password" required>
+                <div class="password-error"></div>
+            </div>
+            <button type="button" class="btn btn-primary" onclick ="validate()">Login</button>
+        </form>
+
+        <div class="side-image-container">
+
+        </div>
